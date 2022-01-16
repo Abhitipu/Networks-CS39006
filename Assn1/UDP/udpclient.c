22d66fc4e6c84ae86be662d3ee9ae3886cb79357
Abhitipu <abhinandan0316@gmail.com>
2022-01-16 15:20:13 +0530

    Added udp code

--- Assn1/UDP/udpclient.c
+++ Assn1/UDP/udpclient.c
@@ -1,39 +1,87 @@
-// A Simple Client Implementation
+/** THE UDP CLIENT **/
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
 #include <string.h> 
+
 #include <sys/types.h> 
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
+#include <fcntl.h>
   
 int main() { 
-    int sockfd; 
-    struct sockaddr_in servaddr; 
-  
-    // Creating socket file descriptor 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if ( sockfd < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
+    if (argc < 2) {
+        printf("Input file not specified!\n");
+        exit(-1);
+    }
+
+    // Creating the socket
+	int	sockfd ;
+	struct sockaddr_in	serv_addr;
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("Unable to create socket\n");
+		exit(-1);
+	}
   
     memset(&servaddr, 0, sizeof(servaddr)); 
+
+    // Opening the file
+    // O_RDONLY flag says that its a read only file
+    int input_fd = open(argv[1], O_RDONLY);
+    
+    if(input_fd < 0) {
+        printf("File not found\n");
+        exit(-1);
+    }
       
-    // Server information 
+    // Server information.. we dont explicitly call connect() here
     servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(8181); 
     inet_aton("127.0.0.1", &serv_addr.sin_addr); 
+    servaddr.sin_port = htons(8181); 
       
     int n;
     socklen_t len; 
-    char *hello = "CLIENT:HELLO"; 
-      
-    sendto(sockfd, (const char *)hello, strlen(hello), 0, 
-			(const struct sockaddr *) &servaddr, sizeof(servaddr)); 
-    printf("Hello message sent from client\n"); 
+    char buf[101] = "Hello from client!\n";
+    sendto(sockfd, (const char *)buf, 100, 0, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
+
+    recvfrom(sockfd, buf, 100, 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+    printf("%s\n", buf);
+
            
+    while(read(input_fd, buf, 100) > 0) {
+        // send the buffer
+	    int check = sendto(sockfd, buf, 100, 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+
+        if(check == -1) {
+            perror("Send failed!\n");
+            exit(-1);
+        }
+
+        // printf("%d\n", ++cnt);
+        printf("%s", buf);
+        memset(buf, '\0', sizeof(buf));
+    }
+
+    printf("Done\n");
+    
+	int parse_status = recvfrom(sockfd, buf, 100, 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+    if(parse_status < 0) {
+        printf("Couldn't send file completely!\n");
+        exit(-1);
+    }
+    
+    printf("%s\n", buf);
+
+    int nWords, nChars, nSentences;
+    int word_status = recvfrom(sockfd, &nWords, sizeof(nWords), 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+    int char_status = recvfrom(sockfd, &nChars, sizeof(nChars), 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+    int sent_status = recvfrom(sockfd, &nSentences, sizeof(nSentences), 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    printf("Words: %d\n", nWords);
+    printf("Characters: %d\n", nChars);
+    printf("Sentences: %d\n", nSentences);
+		
     close(sockfd); 
     return 0; 
 } 
--- Assn1/UDP/udpserver.c
+++ Assn1/UDP/udpserver.c
@@ -1,4 +1,4 @@
-// A Simple UDP Server that sends a HELLO message
+/** THE UDP SERVER**/
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
@@ -8,15 +8,13 @@
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
   
-#define MAXLINE 1024 
-  
 int main() { 
     int sockfd; 
     struct sockaddr_in servaddr, cliaddr; 
       
     // Create socket file descriptor 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if ( sockfd < 0 ) { 
+    if (sockfd < 0) { 
         perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
     } 
@@ -29,23 +27,84 @@ int main() {
     servaddr.sin_port = htons(8181); 
       
     // Bind the socket with the server address 
-    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ) 
-    { 
+    if(bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ) { 
         perror("bind failed"); 
         exit(EXIT_FAILURE); 
     } 
     
-    printf("\nServer Running....\n");
-  
-    int n; 
+
     socklen_t len;
-    char buffer[MAXLINE]; 
+    char buf[101]; 
+    memset(buf, '\0', sizeof(buf));
+
+	recvfrom(sockfd, (char *)buf, 100, 0, (struct sockaddr *) &cliaddr, &len);
+    printf("%s", buf);
+
+    memset(buf, '\0', sizeof(buf));
+    strcpy(buf, "Hello from server!\n");
+    sendto(sockfd, buf, 100, 0, (struct sockaddr *) &cliaddr, &len);
  
     len = sizeof(cliaddr);
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 0, 
-			( struct sockaddr *) &cliaddr, &len); 
-    buffer[n] = '\0'; 
-    printf("%s\n", buffer); 
+    while(1) {
+        int cur_len;
+        int nWords, nChars, nSentences;
+        int lastWasLetter = 0;
+        nWords = nChars = nSentences = 0;
+
+        int toEnd = 0;
+        while(toEnd == 0) {
+            memset(buf, '\0', sizeof(buf));
+
+		    int check = recvfrom(sockfd, (char *)buf, 100, 0, (struct sockaddr *) &cliaddr, &len);
+
+            if(check == -1) {
+                perror("Error in receiving\n");
+                exit(-1);
+            }
+            buf[check] = '\0';
+            printf("%s\n", buf);
+
+            // end of stream
+            if(check == 0)
+                break;
+
+		    printf("%s", buf);
+            cur_len = strlen(buf);
+
+            for(int i = 0; i < cur_len && buf[i] != '\0'; i++) {
+                if(buf[i] == ' ' || buf[i] == '.') {
+                    if(lastWasLetter)
+                        nWords++;
+
+                    lastWasLetter = 0;
+                    nSentences += (buf[i] == '.');
+                }
+                else if(buf[i] != '\0') {
+                    if(buf[i] != '\n')
+                        lastWasLetter = 1;
+
+                    nChars++;
+                }
+            }
+
+            if(cur_len < 3) {
+                toEnd = 1;
+            } else {
+                if(buf[cur_len - 3] == '.' && buf[cur_len - 2] == '.')
+                    toEnd = 1;
+            }
+        }
+
+        nSentences--;
+
+        memset(buf, '\0', sizeof(buf));
+        strcpy(buf, "Parsing complete! Returning results\n");
+        sendto(sockfd, buf, 100, 0, (struct sockaddr *) &cliaddr, &len);
+
+        sendto(sockfd, &nWords, sizeof(nWords), 0, (struct sockaddr *) &cliaddr, &len);
+        sendto(sockfd, &nChars, sizeof(nChars), 0, (struct sockaddr *) &cliaddr, &len);
+        sendto(sockfd, &nSentences, sizeof(nSentences), 0, (struct sockaddr *) &cliaddr, &len);
+    }
     close(sockfd);
     return 0; 
 }
